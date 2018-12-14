1d381dbd561ef1bb70e629a9644b3e8b85947ee5
tatianacv <42974231+tatianacv@users.noreply.github.com>
2018-12-13 23:53:35 -0400

    bluetooth

---
+++ Registro_Comunitario/Registro_Comunitario/CUUIDDefinitions.swift
@@ -0,0 +1,13 @@
+//
+//  CUUIDDefinitions.swift
+//  Bluetooth_v3.0
+//
+//  Created by Juan Lugo on 12/13/18.
+//  Copyright © 2018 manin. All rights reserved.
+//
+
+import CoreBluetooth
+
+let transferServiceUUID = CBUUID(string: "32782995-7CAA-4EA0-B7F7-AF62B051B107")
+let transferCharacteristicUUID = CBUUID(string: "801D80F2-A4AF-4F65-A071-3857575DA3A7")
+let NOTIFY_MTU = 20
